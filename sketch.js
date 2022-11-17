0e8aeb52f884096b951fc2140a8275de01ae2154
Gustav Paragas <gep21b@fsu.edu>
2022-11-17 09:39:03 -0500

    Final Project Update

--- sketch.js
+++ sketch.js
@@ -1,18 +1,52 @@
-let y = 0;
-let speed = 3;
+myCounter = 220;
+myRandom = 80;
 
 function setup() {
-  createCanvas(900, 900);
+  createCanvas(1500,730);
 }
 
 function draw() {
-  background(125);
-
-  ellipse(width/2, y, 50);
-
-  y += speed;
-  if (y > height || y < 0) {
-    speed *= -1;
+  background("black");
+  if (myCounter < myRandom) {
+    fill(220)
+  }else {
+    fill("gold")
   }
+  textSize(150);
+  textFont('Times New Roman');
+  text('VOTE', 250, 200);
 
+  textSize (150);
+  textFont('Times New Roman');
+  text('NOW', 700, 200);
+
+  textSize (150);
+  textFont('Times New Roman');
+  text('for', 450, 325);
+
+  textSize (150);
+  textFont('Times New Roman');
+  text('the', 700, 325);
+
+  textSize (150);
+  textFont('Times New Roman');
+  text('future', 210, 460);
+
+  textSize (150);
+  textFont('Times New Roman');
+  text('of', 580, 460);
+
+  textSize (150);
+  textFont('Times New Roman');
+  text('America', 720, 460);
+
+
+  myCounter++
+  if(myCounter >100){
+    myCounter = 0
+    myRandom = random(10,90);
+  }
+  
 }
+
+
